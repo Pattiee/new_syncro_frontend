0f9d816a3e6f2aa510b48cfb005eae45845d0a29
Patrick Kiplagat <patrickkoros0@gmail.com>
2025-09-13 00:02:40 +0300

    updated product schema

---
+++ src/components/Product/MultipleFileInput.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Controller, useFormContext } from 'react-hook-form'
+
+const MultipleFileInput = ({ name }) => {
+    const { control } = useFormContext();
+
+    return (
+        <Controller
+            control={control}
+            name={name}
+            render={({ field: {onChange, onBlur}, fieldState: {  error} }) => (
+                <div>
+                    <input type="file"
+                    multiple
+                    accept="image/*"
+                    onBlur={onBlur}
+                    onChange={(e) => {
+                        const files = Array.from(e.target.files);
+                        onChange(files);
+                    }}
+                    className="input-field" />
+                    {error && <span className='text-red-500 text-sm mt-1'>{error.message}</span>}
+                </div>
+            )}
+        />
+    )
+}
+
+export default MultipleFileInput
--- src/pages/admin/AddProduct.jsx
+++ src/pages/admin/AddProduct.jsx
@@ -1,19 +1,34 @@
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { productSchema } from '../../schemas/product.schema';
 import { addProduct, getCategories } from '../../services/products.service';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import MultipleFileInput from '../../components/Product/MultipleFileInput'
 
 const AddProduct = () => {
   const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
   const [submitting, setSubmitting] = useState(false);
 
-  const { register, handleSubmit, reset, formState: { errors }, } = useForm({
+  const methods = useForm({
     resolver: zodResolver(productSchema),
+    defaultValues: {
+      percent_discount: 0,
+      stock: 1,
+      images: [],
+    }
   });
+
+  // const { methods.register, handleSubmit, reset, formState: { methods.errors }, } = useForm({
+  //   resolver: zodResolver(productSchema),
+  //   defaultValues: {
+  //     percent_discount: 0,
+  //     stock: 1,
+  //     images: [],
+  //   }
+  // });
 
   useEffect(() => {
     const loadData = async () => {
@@ -43,7 +58,7 @@ const AddProduct = () => {
     setSubmitting(true);
     await addProduct(data).then(res => {
       toast.success('Product added successfully!');
-      reset();
+      methods.reset();
       navigate('/');
     }).catch(err => {
       toast.error(err?.message || 'Error adding product');
@@ -53,33 +68,34 @@ const AddProduct = () => {
   };
 
   return (
-    <div className="max-w-4xl p-6 mx-auto bg-white rounded-lg shadow-xl dark:bg-gray-900 sm:p-8 md:p-10 lg:p-12">
+    <FormProvider {...methods} className="max-w-4xl p-6 mx-auto bg-white rounded-lg shadow-xl dark:bg-gray-900 sm:p-8 md:p-10 lg:p-12">
       <h2 className="mb-8 text-4xl font-semibold text-center text-orange-600 dark:text-orange-400">
         Add New Product
       </h2>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+      <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-6">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
           <div>
             <input
               type="text"
               placeholder="Product Name"
-              {...register('name')}
+              {...methods.register("name")}
               className="input-field"
             />
-            {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+            {methods.formState.errors.name && <p className="text-red-500">{methods.formState.errors.name.message}</p>}
           </div>
 
           <div>
-            <select {...register('category')} className="input-field">
+            <select {...methods.register('category')} className="input-field">
               <option value="">Select category</option>
-              {categories.map((category) => (
-                <option key={category?.id} value={category.name}>
-                  {category.name}
+              {categories.map((category, idx) => (
+                <option className='flex px-4' key={category?.id} value={category?.name || 'Unknown'}>
+                  <p className='bg-orange-500'>{idx + 1}. </p>
+                  <p>{category?.name}</p>
                 </option>
               ))}
             </select>
-            {errors.category && <p className="text-red-500">{errors.category.message}</p>}
+            {methods.formState.errors.category && <p className="text-red-500">{methods.formState.errors.category.message}</p>}
           </div>
         </div>
 
@@ -87,21 +103,18 @@ const AddProduct = () => {
           <div>
             <input
               type="number"
+              min={0}
+              max={50}
               placeholder="Discount (%)"
-              {...register('percent_discount')}
+              {...methods.register('percent_discount')}
               className="input-field"
             />
-            {errors.percent_discount && <p className="text-red-500">{errors.percent_discount.message}</p>}
+            {methods.formState.errors.percent_discount && <p className="text-red-500">{methods.formState.errors.percent_discount.message}</p>}
           </div>
 
           <div>
-            <input
-              type="text"
-              placeholder="Image URL"
-              {...register('image_url')}
-              className="input-field"
-            />
-            {errors.image_url && <p className="text-red-500">{errors.image_url.message}</p>}
+            <label htmlFor="images">Images</label>
+            <MultipleFileInput name={"images"}/>
           </div>
         </div>
 
@@ -109,45 +122,47 @@ const AddProduct = () => {
           <div>
             <input
               type="number"
+              min={0}
               placeholder="Price"
-              {...register('price')}
+              {...methods.register('price')}
               className="input-field"
             />
-            {errors.price && <p className="text-red-500">{errors.price.message}</p>}
+            {methods.formState.errors.price && <p className="text-red-500">{methods.formState.errors.price.message}</p>}
           </div>
 
           <div>
-            <select {...register('condition')} className="input-field">
+            <select {...methods.register('condition')} className="input-field">
               <option value="">Select condition</option>
-              <option value="New">New</option>
-              <option value="Refurbished">Refurbished</option>
+              <option value="New">1. New</option>
+              <option value="Refurbished">2. Refurbished</option>
             </select>
-            {errors.condition && <p className="text-red-500">{errors.condition.message}</p>}
+            {methods.formState.errors.condition && <p className="text-red-500">{methods.formState.errors.condition.message}</p>}
           </div>
         </div>
 
         <div>
           <textarea
             placeholder="Product description"
-            {...register('desc')}
+            {...methods.register('desc')}
             className="h-32 resize-none input-field"
           />
-          {errors.desc && <p className="text-red-500">{errors.desc.message}</p>}
+          {methods.formState.errors.desc && <p className="text-red-500">{methods.formState.errors.desc.message}</p>}
         </div>
 
         <div className="flex items-center gap-2">
-          <input type="checkbox" {...register('featured')} className="w-6 h-6 text-orange-600" />
+          <input type="checkbox" {...methods.register('featured')} className="w-6 h-6 text-orange-600" />
           <label className="text-lg text-gray-700 dark:text-gray-200">Featured Product</label>
         </div>
 
         <div>
           <input
             type="number"
+            min={1}
             placeholder="Stock"
-            {...register('stock')}
+            {...methods.register('stock')}
             className="input-field"
           />
-          {errors.stock && <p className="text-red-500">{errors.stock.message}</p>}
+          {methods.formState.errors.stock && <p className="text-red-500">{methods.formState.errors.stock.message}</p>}
         </div>
 
         <button
@@ -158,7 +173,7 @@ const AddProduct = () => {
           Add Product
         </button>
       </form>
-    </div>
+    </FormProvider>
   );
 };
 
