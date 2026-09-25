98cc9e4bd6759a93ef2f17693d9381bf642b0337
Patrick Kiplagat <72331075+Pattiee@users.noreply.github.com>
2026-09-25 18:22:36 +0300

    Delete useAuth.js
    already created typescript version of useAuth

--- src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-// src/hooks/useAuth.js
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) throw new Error("UserContext not found");
-  return context;
-};
