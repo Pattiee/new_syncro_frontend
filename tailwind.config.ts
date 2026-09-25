d1a57b0f1719b47d2e593f95d94188039969fb98
Patrick Kiplagat <72331075+Pattiee@users.noreply.github.com>
2026-09-25 23:15:51 +0300

    Create tailwind.config.ts
    typescript version of tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    "./src/**/*.{ts,tsx}"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        'slide-in': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        slide: 'slide 20s linear infinite',
+        'slide-in': 'slide-in 1s ease-out forwards',
+        'fade-in': 'fade-in 1s ease-in forwards',
+      },
+      colors: {
+        primary: '#ff6600'
+      },
+      transitionProperty: {
+        'bg-color': 'background-color, color',
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+};
+
+export default config;
